d5a1719d38a6b3f2cf6418bc64dff0e6c82b35b5
b50789 <acarlton@fullsail.edu>
2013-10-03 20:16:10 -0500

    Done I think

--- Project/js/json.js
+++ Project/js/json.js
@@ -1,6 +1,9 @@
-// Project 4 Web App
-// VFW 1309
-// Adam Carlton
+/* 
+	Project 1
+	Adam Carlton
+	MIU 1310
+*/
+
 
 var json = {
 	"tvShow1": {
@@ -32,7 +35,178 @@ var json = {
 		"block_yes":["Parental Block Needed?", "Yes"],
 		"specialnotes":["Notes:", "Another Great TV Show"],
 
+	},
+	"tvShow4": {
+		"date":["Date:", "2013-09-27"],
+		"showname":["Show Name:", "The Mentalist Season 5"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow5": {
+		"date":["Date:", "2013-10-29"],
+		"showname":["Show Name:", "The Carlton Show"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow6": {
+		"date":["Date:", "2013-09-12"],
+		"showname":["Show Name:", "The Dog Show"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow7": {
+		"date":["Date:", "2014-09-29"],
+		"showname":["Show Name:", "The FSO Show"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow8": {
+		"date":["Date:", "2013-11-28"],
+		"showname":["Show Name:", "The Turkey Show"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow9": {
+		"date":["Date:", "2013-10-10"],
+		"showname":["Show Name:", "The Brian-Austin Show"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow10": {
+		"date":["Date:", "2013-09-29"],
+		"showname":["Show Name:", "The Terry Show"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow11": {
+		"date":["Date:", "2013-09-29"],
+		"showname":["Show Name:", "The Hello Show"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow12": {
+		"date":["Date:", "2013-10-29"],
+		"showname":["Show Name:", "The Tv Show"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow13": {
+		"date":["Date:", "2013-12-25"],
+		"showname":["Show Name:", "Christmas"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow14": {
+		"date":["Date:", "2013-12-29"],
+		"showname":["Show Name:", "The New Year Show"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow15": {
+		"date":["Date:", "2014-01-29"],
+		"showname":["Show Name:", "The Walking Talking Show"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow16": {
+		"date":["Date:", "2014-03-19"],
+		"showname":["Show Name:", "The Best TV Show Ever"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow17": {
+		"date":["Date:", "2014-12-15"],
+		"showname":["Show Name:", "The Fake Show"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow18": {
+		"date":["Date:", "2015-09-29"],
+		"showname":["Show Name:", "The Fullsail Show"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow19": {
+		"date":["Date:", "2013-12-24"],
+		"showname":["Show Name:", "The Carlton Christmas Show"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
+	},
+	"tvShow20": {
+		"date":["Date:", "2013-12-12"],
+		"showname":["Show Name:", "The Last Data"],
+		"r_yes":["Recurring Show:", "Yes"],
+		"networks":["'Network:", "CBS"],
+		"important":["How Important?", "100"],
+		"block_yes":["Parental Block Needed?", "Yes"],
+		"specialnotes":["Notes:", "Another Great TV Show"],
+
 	}
+
 
 
 }
